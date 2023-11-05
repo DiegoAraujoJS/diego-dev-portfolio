f0d54d447319a1bc8d5c920158d74009619bd283
Diego Araujo <diegolaraujo96@gmail.com>
2023-11-05 02:31:37 -0300

    Logic of scroller

--- app/routes/portfolio/route.tsx
+++ app/routes/portfolio/route.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useRef, useState } from "react";
 import Contact from "~/components/index/Contact";
 import Presentation from "~/components/index/Presentation";
 import Proyects from "~/components/index/Proyects";
@@ -6,19 +7,22 @@ import Proyects from "~/components/index/Proyects";
 export const meta: MetaFunction = () => {
   return [
     { title: "Diego Araujo Developer" },
-    { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 
 export default function Index() {
+  const [scrollPosition, setScrollPosition] = useState(0)
   return (
-    <div className="h-full snap-y snap-mandatory overflow-scroll">
+    <div className="h-full snap-y snap-mandatory overflow-scroll" onScroll={(e: any) => {
+      if (e.target.scrollTop % e.target.offsetHeight === 0) {
+        const pageLength = e.target.offsetHeight
+        setScrollPosition([0, 1, 2].find(page => pageLength * page === e.target.scrollTop) || 0)
+      }
+    }}>
 
       <ul className="steps steps-vertical fixed bottom-5">
-        <li className="step step-primary"></li>
-        <li className="step"></li>
-        <li className="step"></li>
+        {[0, 1, 2].map((step, i) => <li key={i} className={`step ${step === scrollPosition ? 'step-primary' : ''}`}></li>)}
       </ul>
 
       <Presentation className="snap-start h-full"/>
