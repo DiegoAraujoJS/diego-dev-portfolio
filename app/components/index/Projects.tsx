d372dfd145b54c830e340be70556d19e8c6ddf2a
Diego Araujo <diegolaraujo96@gmail.com>
2023-11-07 02:01:57 -0300

    enriched projects section. Added qr icons

--- app/components/index/Projects.tsx
+++ app/components/index/Projects.tsx
@@ -1,9 +1,24 @@
-// function Carousel({className} : {
-//   className?: string
-// }) {
-//   return (
-//   )
-// }
+function CarouselItem({title, imgSrc, description} : {
+  title: string
+  imgSrc: string
+  description: string[]
+}) {
+  return (
+    <div className="carousel-item w-full">
+      <div className="flex w-full">
+        <div className="">
+          <p className="text-3xl text-center w-full">{title}</p>
+          <img src={imgSrc} className="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div className="flex-1">
+          {description.map((paragraph, i) => [<p key={i} className="px-10 pr-40 py-4">{paragraph}</p>]).flat()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const dummy = ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis deserunt iusto alias quasi eaque assumenda eveniet qui fugiat quod necessitatibus et ab corporis, dignissimos nesciunt in, voluptas nostrum labore autem?", "Commodi quis vero sequi nulla numquam aut laborum dolores eos? Veritatis doloremque distinctio culpa. Ipsam ab voluptas velit tempora deserunt. Incidunt odio aperiam explicabo fuga omnis quidem laboriosam quod numquam?", "Ipsa suscipit, minus illo quidem veniam doloremque quas, error fuga labore eligendi porro ea expedita rerum minima, et voluptatem exercitationem! Autem incidunt perspiciatis animi non dolorum ab saepe dignissimos labore?"]
 
 export default function Projects({className, id} : {className?: string, id?: string}) {
   return (
@@ -14,19 +29,8 @@ export default function Projects({className, id} : {className?: string, id?: str
 
       <div className={`carousel rounded-box border border-solid border-black w-full`}>
 
-        <div className="carousel-item w-full">
-          <div className="border border-solid border-white">
-            <p className="text-3xl border border-solid border-white text-center w-full">Discord Jira Bot</p>
-            <img src="/portfolio/discord.png" className="w-full" alt="Tailwind CSS Carousel component" />
-          </div>
-        </div> 
-
-        <div className="carousel-item w-full">
-          <div className="border border-solid border-white">
-            <p className="text-3xl border border-solid border-white text-center w-full">Git Deploy</p>
-            <img src="/portfolio/git.png" className="w-full" alt="Tailwind CSS Carousel component" />
-          </div>
-        </div>
+        <CarouselItem title="Discord Jira Bot" imgSrc="/portfolio/discord.png" description={dummy}/> 
+        <CarouselItem title="Git Deploy" imgSrc="/portfolio/git.png" description={dummy} />
 
       </div>
 
